514db53e74bb7dfd26403a775b8773ec2ff0910f
bin.yao <bin.yao@yeahmobi.com>
2017-05-16 17:15:57 +0800

    添加cors配置

--- server/src/main/java/com/skyblue/coalapp/server/Application.java
+++ server/src/main/java/com/skyblue/coalapp/server/Application.java
@@ -2,12 +2,26 @@ package com.skyblue.coalapp.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Created by Administrator on 2017/4/12.
  */
 @SpringBootApplication
 public class Application {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
 
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
