969f36df113f641668d4a611b916ec644fd43b19
lengque <258236290@qq.com>
2017-05-16 23:02:44 +0800

    test upload file

--- server/src/main/java/com/skyblue/coalapp/server/Application.java
+++ server/src/main/java/com/skyblue/coalapp/server/Application.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
- * Created by Administrator on 2017/4/12.
+ *  sprint boot start up
  */
 @SpringBootApplication
 public class Application {
