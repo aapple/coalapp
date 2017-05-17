db45726470ba2ca6adff47192363483efc45dd08
bin.yao <bin.yao@yeahmobi.com>
2017-05-17 10:39:19 +0800

    添加cors配置

--- server/src/main/java/com/skyblue/coalapp/server/example/controller/CityController.java
+++ server/src/main/java/com/skyblue/coalapp/server/example/controller/CityController.java
@@ -3,15 +3,15 @@ package com.skyblue.coalapp.server.example.controller;
 import com.skyblue.coalapp.server.example.service.CityService;
 import com.skyblue.coalapp.server.repsonse.ResponseMessage;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by yaobin on 2017/4/28.
  */
-@Controller
+@RestController
 @RequestMapping("/city")
 public class CityController {
 
