2a64738d8a3da29df34f6d68414962789d4d8ac4
lengque <258236290@qq.com>
2017-05-28 15:28:24 +0800

    信息部

--- server/src/main/java/com/skyblue/coalapp/server/Information/controller/InfoDepartController.java
+++ server/src/main/java/com/skyblue/coalapp/server/Information/controller/InfoDepartController.java
@@ -3,8 +3,6 @@ package com.skyblue.coalapp.server.Information.controller;
 import com.alibaba.fastjson.JSON;
 import com.skyblue.coalapp.server.Information.domain.InfoDepartment;
 import com.skyblue.coalapp.server.Information.service.InfoDepartService;
-import com.skyblue.coalapp.server.Information.vo.InfoDepartmentVO;
-import com.skyblue.coalapp.server.user.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,25 +19,15 @@ import java.util.List;
 public class InfoDepartController {
 
     @Autowired
-    InfoDepartService infoDepartService;
+    private InfoDepartService infoDepartService;
 
     @RequestMapping("/addOrUpdate")
-    public void addOrUpdate(InfoDepartment infoDepart){
-        infoDepart =  new InfoDepartment();
-        infoDepart.setTitle("testUser");
-        User user = new User();
-        user.setId(2);
-        infoDepart.setUser(user);
-        infoDepart.setAddress("test");
+    public void addOrUpdate(@RequestBody InfoDepartment infoDepart){
         infoDepartService.addInfoDepartment(infoDepart);
     }
 
     @RequestMapping("/getInfoDepartment")
     public String findInfoDepartment(@RequestBody InfoDepartment infoDepart){
-        /*infoDepart =  new InfoDepartment();
-        User user = new User();
-        user.setId(1);
-        infoDepart.setUser(user);*/
         InfoDepartment infoDepartment = infoDepartService.findOne(infoDepart);
 
         return JSON.toJSONString(infoDepartment);
