399310e774fa71969035a1843cb4eb747410e0b2
lengque <258236290@qq.com>
2017-07-02 23:52:46 +0800

    新增新闻模块

--- server/src/main/java/com/dayuzl/coalapp/server/dailynews/controller/DailyNewsController.java
+++ server/src/main/java/com/dayuzl/coalapp/server/dailynews/controller/DailyNewsController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
-@RequestMapping("/app/traffic")
+@RequestMapping("/app/news")
 public class DailyNewsController {
 
     Logger logger = LoggerFactory.getLogger(getClass());
